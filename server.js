ca70f421a0e4e5256912bb53c01d63afdc01931b
dia-7691 <entrydia7691@gmail.com>
2020-11-02 19:49:55 +0900

    다크모드(Dark-Mode) 추가

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const os = require('os')
+const path = require('path').resolve()
+const { renderFile: render } = require('ejs')
+
+app.use('/src', express.static(path + '/src'))
+app.get('/', async (_, res) => {
+    let cpuName = os.cpus()
+    const str = await render(path + '/views/index.ejs', { 
+        hostname: os.hostname(), 
+        type: os.type(), 
+        platform: os.platform(),
+        cpu: cpuName[1].model, 
+        rem: Math.floor(os.totalmem()/1000000000),
+        uptime: Math.floor(os.uptime()/60)
+    })
+    res.send(str)
+    // console.log(cpuName[1])
+})
+
+app.get('/dark', async (_, res) => {  //dark mode
+    let cpuName = os.cpus()
+    const str = await render(path + '/views/index-dark.ejs', { 
+        hostname: os.hostname(), 
+        type: os.type(), 
+        platform: os.platform(),
+        cpu: cpuName[1].model, 
+        rem: Math.floor(os.totalmem()/1000000000),
+        uptime: Math.floor(os.uptime()/60)
+    })
+    res.send(str)
+    // console.log(cpuName[1])
+})
+
+app.listen(8080, () => console.log('Server on : http://localhost:8080/'))
